7bea556768f3ff11d86c27dd5ccabe9acc8755d8
alfaAloneH <fitoalfavzq@gmail.com>
2017-06-15 14:31:18 -0500

    Create JavaApplication47.java
    Primer cambio desde GitHub

--- src/javaapplication47/JavaApplication47.java
+++ src/javaapplication47/JavaApplication47.java
@@ -16,6 +16,8 @@ public class JavaApplication47 {
      */
     public static void main(String[] args) {
         System.out.print("Hola mundo");
+        Scanner s =new Scanner(System.in);
+        
     }
     
 }
